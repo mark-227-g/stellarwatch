9a2acd0794bf21d17ba23960f142e85ebb8c3c05
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-05-07 15:33:11 -0400

    fixed home js

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -23,21 +23,10 @@ function addEventHandlers()
   });
 }
 
-addEventToUser(id)
-try {
-  // Get all Homepages, sorted by id
-  const astroEventData = await AstroEvent.findAll({
-    order: [['id', 'ASC']],
-  });
+function addEventToUser(id) {
+alert('add event to user'+id);
+};
 
-  // Serialize Homepage data so templates can read it
-  const astroEvents = astroEventData.map((project) => project.get({ plain: true }));
-
-  // Pass serialized data into Handlebars.js template
-  res.render('homepage', { astroEvents });
-} catch (err) {
-  res.status(500).json(err);
-}
 /************************************** 
 Event handler for viewButtonClick
 **************************************/
