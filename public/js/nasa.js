0c216199e2c4ee964f36049bcdfb464a3d5c6f86
Zach Berger <zachhberger@gmail.com>
2023-05-09 13:48:28 -0400

    updated styling and create account page

--- public/js/nasa.js
+++ public/js/nasa.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const nasaImage = document.querySelector('#nasa-image');
 	const nasaTitle = document.querySelector('.nasa-title');
+	const nasaLink = document.createElement('a');
   
-	fetch('https://api.nasa.gov/planetary/apod?api_key=OD0qF5NTV5kD4eE0fvvCLVFCIfJ4cz8wAGE9h5aO')
+	fetch(`https://api.nasa.gov/planetary/apod?api_key=OD0qF5NTV5kD4eE0fvvCLVFCIfJ4cz8wAGE9h5aO`)
 	  .then(response => response.json())
 	  .then(data => {
 		const image = document.createElement('img');
@@ -10,9 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
 		image.alt = data.title;
 		nasaImage.appendChild(image);
 		nasaTitle.textContent = data.title;
+		const date = new Date(data.date).toLocaleDateString('en-US', { month: 'numeric', day: 'numeric', year: 'numeric' });
+		nasaLink.textContent = `NASA Astronomy Image of the Day [${date}]`;
+		nasaLink.href = 'https://apod.nasa.gov/apod/astropix.html';
+		nasaLink.target = '_blank';
+		nasaTitle.appendChild(document.createElement('br'));
+		nasaTitle.appendChild(nasaLink);
 	  })
 	  .catch(error => {
 		console.error(error);
 		nasaImage.textContent = 'Error loading image';
 	  });
-});
+  });
+  
