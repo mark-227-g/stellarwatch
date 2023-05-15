3a3b560fd3ae2f8acac03d652238f760cc394574
Zach Berger <zachhberger@gmail.com>
2023-05-15 17:33:09 -0400

    fixed some issues caused by heroku deployment

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const stellarUser = require('../models/stellarUser.js');
 
 router.get('/', (req, res) => {
